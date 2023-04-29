5ec256cde82e679ae3511fbc9e3782dcf80df842
Richeal Akushika Allotey <122970398+AkushikaRA@users.noreply.github.com>
2023-04-29 14:20:12 +0000

    Delete WDD230 directory

--- WDD230/scipts/myScript.js
+++
@@ -1,5 +0,0 @@
-//Function to get current year
-document.getElementById('currentYear').innerHTML = new Date().getFullYear();
-
-//Get document last modified date
-document.getElementById('lastUpdated').innerHTML = "Last Updated: " + document.lastModified;
